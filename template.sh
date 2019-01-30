934d7b6405fdacb2717a9ee2d29e970cdc5491e3
Nils Nathorst-Windahl <38070556+NilsNathorst@users.noreply.github.com>
2019-01-30 13:05:58 +0100

    Update template.sh

--- template.sh
+++ template.sh
@@ -7,7 +7,7 @@ echo ""
 	exit
 fi
 echo "Creating new app in $(pwd)/$1"
-git clone &> /dev/null https://github.com/nilsWithSkills/rollup-boilerplate.git $1
+git clone &> /dev/null https://github.com/NilsNathorst/rollup-boilerplate.git $1
 cd $1
 rm -rf .git
 git init
